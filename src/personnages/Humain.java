722c5100e04e3405b9717b6c8a4ea64fd0ae2da5
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2024-03-26 19:25:57 +0100

    v0.2 fonction main de humain

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -6,12 +6,12 @@ public class Humain {
 	private String nom;
 	private String boissonPref;
 	private int qntArgent;
-	private int qntArgentMax = 100;
+	private int qntArgentMax;
 
 	public Humain(String nom, String boissonPref, int qntArgentMax) {
 		this.nom = nom;
 		this.boissonPref = boissonPref;
-		this.qntArgentMax = qntArgentMax;
+		this.qntArgentMax = 100;
 		this.qntArgent = genererQntArgent();
 	}
 
@@ -38,23 +38,24 @@ public class Humain {
 	}
 
 	public void direBonjour() {
-		parler("Bonjour! Je m'appelle" + nom + "et j’aime boire du" + boissonPref);
+		parler("Bonjour! Je m'appelle" + nom + "et j’aime boire du " + boissonPref);
 	}
 
 	public void parler(String texte) {
-		System.out.println("(" + nom + ")" + texte);
+		System.out.println("(" + nom + ") " + texte);
 	}
 
-	public void boire() {
-		parler("Mmmm, un bon verre de" + boissonPref + "! GLOUPS !");
+	public void boire(String boisson) {
+		parler("Mmmm, un bon verre de " + boisson + "! GLOUPS !");
 	}
 
 	public void acheter(String bien, int prix) {
 		if (prix < qntArgent) {
-			parler("J'ai" + qntArgent + "sous en poche" + "Je vais pouvoir m'orir une boisson à" + prix + "sous");
+			parler("J'ai " + qntArgent + " sous en poche " + "Je vais pouvoir m'orir une boisson à " + prix + " sous");
+			boire(bien);
 			perdreArgent(prix);
 		} else {
-			parler("Je n'ai plus que " + qntArgent + "sous en poche" + "Je ne peux même pas m'orir un" + bien + "à" + prix + "sous")
+			parler("Je n'ai plus que " + qntArgent + " sous en poche. Je ne peux même pas m'offrir un " + bien + " à " + prix + " sous");
 		}
 	}
 
@@ -66,4 +67,14 @@ public class Humain {
 		qntArgent -= perte;
 
 	}
+	
+    public static void main(String[] args) {
+        Humain humain = new Humain("Jean", "Coca", 10);
+
+        humain.direBonjour(); 
+ 
+        humain.acheter("café", 2); 
+        humain.acheter("marihuana tea", 100);
+
+    }
 }
