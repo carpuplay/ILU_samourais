f067d941e8e5fbe69e495f431f6045d0e8aad8a3
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2024-03-27 22:46:22 +0100

    v0.4 Save with errors

--- src/personnages/Yazuka.java
+++ src/personnages/Yazuka.java
@@ -8,9 +8,21 @@ public class Yazuka extends Humain{
 	public Yazuka(String nom, String boissonPref, int qntArgentMax, String clan, int reputation) {
 		super(nom, boissonPref, qntArgentMax);
 		this.reputation = 0;
-		this.clan
+		this.clan = clan;
 	}
 	
+	public void extorquer(Commercant commercant) {
+		int argentExtorque = commercant.seFaireExtorquer();
+		parler(" Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
+		parler(commercant + ", si tu tiens à la vie donne moi ta bourse ");
+		parler(" J’ai piqué les " + argentExtorque + " sous de Marco, ce qui me fait" + getQntArgent() + " sous dans ma\n"
+				+ "poche. Hi ! Hi !");
+	}
+	
+    public static void main(String[] args) {
+    	Yazuka yakuza = new Yakuza("Pepe", null, 0) ;
+
+    }
 	
 	
 }
