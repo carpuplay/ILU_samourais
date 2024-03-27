8c0fdc53fe34d1d2410cd090b8f7bcf25a01e704
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2024-03-27 21:09:47 +0100

    aa

---
+++ src/personnages/Yazuka.java
@@ -0,0 +1,16 @@
+package personnages;
+
+public class Yazuka extends Humain{
+
+	private int reputation;
+	private String clan;
+	
+	public Yazuka(String nom, String boissonPref, int qntArgentMax, String clan, int reputation) {
+		super(nom, boissonPref, qntArgentMax);
+		this.reputation = 0;
+		this.clan
+	}
+	
+	
+	
+}
